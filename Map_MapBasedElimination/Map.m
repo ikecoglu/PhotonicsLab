a59dde7292ebf899c98ffa052b9fca6212e97ec6
İbrahim Keçoğlu <42060188+ikecoglu@users.noreply.github.com>
2021-08-04 09:46:35 +0300

    Added mat file reading

--- Map_MapBasedElimination/Map.m
+++ Map_MapBasedElimination/Map.m
@@ -2,43 +2,39 @@ clear all;close all; clc;
 %% User input
 
 Length=60; %legth of a side of scanned square. Unit is how many our step so actually legth/stepsize.
-Point2map=524;%the raman shift that is going to be mapped in 3d plot.
+Point2map=1002;%the raman shift that is going to be mapped in 3d plot.
 n=2;%averaging window size.
 %% Reading data
 
-[fileName, pathName] = uigetfile('*.*','Select data to map.','MultiSelect', 'on');
-FileName=fullfile(pathName,fileName);
-dataSize=length(FileName);
-for i=1:dataSize
-    clc;
-    disp(strcat("Reading data: ",int2str((i/dataSize)*100),"%"));
-    data(i,:,:)=dlmread(FileName{1,i,:},",");
-end
+[fileName, pathName] = uigetfile('*.mat*','Select data to map.');
+FileName = fullfile(pathName,fileName);
+load(FileName)
+dataSize = size(CalInt, 1);
 %% Process
 
-peak=find(data(1,:,1)==Point2map);
+peak=find(Calx == Point2map);
 peak=peak-n:peak+n;
 i=0;b=0;counter=0;Gx=Length;Gy=Length;
-img(Gy+1,Gx+1)=mean(data(1,peak,2));
+img(Gy+1,Gx+1)=mean(CalInt(1,peak));
 while i<dataSize
     a=mod(b,4);
     if(a==0)
         for i=counter*(Length+1)+2:(counter+1)*(Length+1)
             Gy=Gy-1;
-            img(Gy+1,Gx+1)=mean(data(i,peak,2));
+            img(Gy+1,Gx+1)=mean(CalInt(i,peak));
         end
         b=b+1;counter=counter+1;
     end
     if(a==1||a==3)
         i=counter*(Length+1)+1;
         Gx=Gx-1;
-        img(Gy+1,Gx+1)=mean(data(i,peak,2));
+        img(Gy+1,Gx+1)=mean(CalInt(i,peak));
         b=b+1;
     end
     if(a==2)
         for i=counter*(Length+1)+2:(counter+1)*(Length+1)
             Gy=Gy+1;
-            img(Gy+1,Gx+1)=mean(data(i,peak,2));
+            img(Gy+1,Gx+1)=mean(CalInt(i,peak));
         end
         b=b+1;counter=counter+1;
     end
@@ -53,4 +49,4 @@ imagesc(img);
 % saveas(gcf,'C:\Users\ibrah\Desktop\Map.emf');close;
 %% Notes from the writer of the code
 
-clc;disp('Always check whether the scanning direction of image created is same as the scanning you have done.')
+clc;disp('Always check whether the scanning direction of image created is the same as the scanning you have done.')
