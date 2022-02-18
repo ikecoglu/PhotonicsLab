d26973a0281dc69938010dcb80a11b09e0a89874
Ibrahim Kecoglu <42060188+ikecoglu@users.noreply.github.com>
2022-02-18 14:20:23 +0300

    variable names

--- BaseCorrection_Normalization/BaseExplorer.m
+++ BaseCorrection_Normalization/BaseExplorer.m
@@ -1,22 +1,22 @@
-clear all;close all;clc;
+% clear all;close all;clc;
 %% Importing data
 
-[FileName, PathName] = uigetfile('*.mat*', 'Select calibrated data');
-FileName_f = fullfile(PathName,FileName);
-load(FileName_f)
+[name, path] = uigetfile('*.mat*', 'Select calibrated data');
+FilePath = fullfile(path,name);
+load(FilePath)
 %% Base selection
-
-BasePoints = [670 780 901 1024 1148 1457 1619 1657 1672 1723 1739 1798 1800];
-data = [ht50thp1; ht50thp5; ht50thp10; ht50thp20; ht50thp25; ht50thp40; ht50thp50];
+close all
+% BasePoints = [407 476 581 663 896 961 1070 1141 1157 1163 1193 1280 1347 1350 1403 1535 1643 1695 1718 1732 1795 1800];
+BasePoints = [476 581 896 961 1141 1157 1163 1193 1280 1347 1350 1403 1535 1643 1695 1718 1732 1795 1800];
 indx_s = find(Calx==BasePoints(1)); indx_f = find(Calx==BasePoints(end));
-data_p = data(:, indx_s:indx_f); Calx_p = Calx(:, indx_s:indx_f);
+data = mean(CalInt(:, indx_s:indx_f),1); Calx_p = Calx(:, indx_s:indx_f);
 
-[BCInt, y] = base_correct(BasePoints, Calx_p, data_p, 'linear');
+[BCInt, y] = base_correct(BasePoints, Calx_p, data, 'linear');
 
 figure, hold on
-plot(Calx_p, data_p, Calx_p, y, 'LineWidth', 1.5)
-minv = round(min(min(data_p)));
-maxv = round(max(max(data_p)));
+plot(Calx_p, data, Calx_p, y, 'LineWidth', 1.5)
+minv = round(min(min(data)));
+maxv = round(max(max(data)));
 for i = 1:size(BasePoints,2)
     plot([BasePoints(i) BasePoints(i)],[minv maxv],'k--');
 end
